fcfcc7245dccfcc7dbf20b50fb0f20a385f6601d
Jaid <jaid.jsx@gmail.com>
2019-01-13 19:10:55 +0100

    Changed formatting

--- build.js
+++ build.js
@@ -12,10 +12,13 @@ import prettyBytes from "pretty-bytes"
 const presets = fs.readdirSync(path.join(__dirname, "packages"))
 
 for (const name of presets) {
+
   console.log(`- ${chalk.green(name)}`)
+
   const presetPath = path.resolve(__dirname, "packages", name)
   const buildPath = path.resolve(__dirname, "build", name)
   const configBuildPath = path.join(buildPath, "index.json")
+
   empSync(buildPath)
   const pkg = jsYaml.safeLoad(fs.readFileSync(path.join(presetPath, "package.yml"), "utf-8"))
   const config = jsYaml.safeLoad(fs.readFileSync(path.join(presetPath, "babel.yml"), "utf-8"))
@@ -31,9 +34,12 @@ for (const name of presets) {
       ...pkg.peerDependencies && filterObj(rootPkg.devDependencies, key => pkg.peerDependencies.includes(key))
     },
   }
+
   fs.outputJsonSync(configBuildPath, config)
   fs.outputJsonSync(path.join(buildPath, "package.json"), generatedPkg)
   fs.copyFileSync("license.txt", path.join(buildPath, "license.txt"))
   fs.copyFileSync("readme.md", path.join(buildPath, "readme.md"))
+
   console.log(`  index.json: ${prettyBytes(fs.statSync(configBuildPath).size)}`)
+
 }
