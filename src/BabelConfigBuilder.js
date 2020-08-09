026bce43fba1d6b5baccbc515700e34a16b34fd6
Jaid <jaid.jsx@gmail.com>
2020-08-09 07:27:29 +0200

    Improved JSDoc comments
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/BabelConfigBuilder.js
+++ src/BabelConfigBuilder.js
@@ -2,6 +2,12 @@ import {sync as readPkgUp} from "read-pkg-up"
 
 export default class BabelConfigBuilder {
 
+  /**
+   * @constructor
+   * @param {object} babelApi
+   * @param {string} [cwd]
+   */
+
   constructor(babelApi, cwd = process.cwd()) {
     this.config = {
       presets: [],
@@ -22,47 +28,101 @@ export default class BabelConfigBuilder {
     }
   }
 
+  /**
+   * @function
+   * @param {string} name
+   * @param {object} [pluginOptions]
+   */
+
   plugin(name, pluginOptions) {
     this.config.plugins.push(pluginOptions ? [name, pluginOptions] : name)
   }
 
-  pluginForEnv(envTest, ...args) {
+  /**
+   * @function
+   * @param {string} envTest
+   * @param {string} name
+   * @param {object?} [presetOptions]
+   */
+
+  pluginForEnv(envTest, name, presetOptions) {
     if (this.babelApi.env(envTest)) {
-      this.plugin(...args)
+      this.plugin(name, presetOptions)
     }
   }
 
-  pluginForEnvsBut(envTest, ...args) {
+  /**
+   * @function
+   * @param {string} envTest
+   * @param {string} name
+   * @param {object?} [presetOptions]
+   */
+
+  pluginForEnvsBut(envTest, name, presetOptions) {
     if (!this.babelApi.env(envTest)) {
-      this.plugin(...args)
+      this.plugin(name, presetOptions)
     }
   }
 
-  pluginForDependency(dependency, ...args) {
+  /**
+   * @function
+   * @param {string} dependency
+   * @param {string} name
+   * @param {object?} [presetOptions]
+   */
+
+  pluginForDependency(dependency, name, presetOptions) {
     if (this.pkgDependencies?.includes(dependency)) {
-      this.plugin(...args)
+      this.plugin(name, presetOptions)
     }
   }
+
+  /**
+   * @function
+   * @param {string} name
+   * @param {object?} [presetOptions]
+   */
 
   preset(name, presetOptions) {
     this.config.presets.push(presetOptions ? [name, presetOptions] : name)
   }
 
-  presetForEnv(envTest, ...args) {
+  /**
+   * @function
+   * @param {string} envTest
+   * @param {string} name
+   * @param {object?} [presetOptions]
+   */
+
+  presetForEnv(envTest, name, presetOptions) {
     if (this.babelApi.env(envTest)) {
-      this.preset(...args)
+      this.preset(name, presetOptions)
     }
   }
 
-  presetForEnvsBut(envTest, ...args) {
+  /**
+   * @function
+   * @param {string} envTest
+   * @param {string} name
+   * @param {object?} [presetOptions]
+   */
+
+  presetForEnvsBut(envTest, name, presetOptions) {
     if (!this.babelApi.env(envTest)) {
-      this.preset(...args)
+      this.preset(name, presetOptions)
     }
   }
 
-  presetForDependency(dependency, ...args) {
+  /**
+   * @function
+   * @param {string} dependency
+   * @param {string} name
+   * @param {object?} [presetOptions]
+   */
+
+  presetForDependency(dependency, name, presetOptions) {
     if (this.pkgDependencies?.includes(dependency)) {
-      this.preset(...args)
+      this.preset(name, presetOptions)
     }
   }
 
