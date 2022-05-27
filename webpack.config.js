14f522adfb0f47a6ab4a98e4aad6f2f3f2a46b9e
Jaid <jaid.jsx@gmail.com>
2022-05-27 20:49:48 +0200

    breaking: Temporarily switching from ESM to UMD2 again
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -2,4 +2,16 @@ import configure from "webpack-config-jaid"
 
 export default configure({
   documentation: true,
+  extra: {
+    experiments: {
+      outputModule: false,
+      topLevelAwait: false,
+    },
+    output: {
+      module: false,
+      library: {
+        type: "umd2",
+      },
+    },
+  },
 })
