8724f96a2a91a000c2385f1aa0f86fa8588fedb7
Jaid <jaid.jsx@gmail.com>
2022-07-07 17:39:17 +0200

    Reset Webpack config
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,3 @@
 import configure from "webpack-config-jaid"
 
-export default configure({
-  documentation: true,
-  extra: {
-    experiments: {
-      outputModule: false,
-      topLevelAwait: false,
-    },
-    output: {
-      module: false,
-      library: {
-        type: "umd2",
-      },
-    },
-  },
-})
+export default configure()
